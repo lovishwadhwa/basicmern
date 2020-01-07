fa3ed8633732dea27ae19eb201e5e02449127a19
Lovish Wadhwa <lovish.wadhwa11@live.com>
2020-01-07 14:58:12 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useUnifiedTopology: true , useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
